19b0780e4dc3d21b207366d1928296f0c6fb5345
pwwambui <pwwambui@safaricom.co.ke>
2025-01-09 12:55:17 +0300

    added error handling

--- src/components/AddPoem.jsx
+++ src/components/AddPoem.jsx
@@ -5,38 +5,59 @@ const AddPoem = ({ onAdd }) => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const [genre, setGenre] = useState('');
+  const [description, setDescription] = useState('');
+  const [error, setError] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onAdd({ title, author, genre });
+
+    if (!title || !author || !genre || !description) {
+      setError('All fields are required!');
+      return;
+    }
+    setError('');
+
+    onAdd({ title, author, genre, description });
+
     setTitle('');
     setAuthor('');
     setGenre('');
+    setDescription('');
   };
 
   return (
     <div className="card" >
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Poem Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Author"
-        value={author}
-        onChange={(e) => setAuthor(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Genre"
-        value={genre}
-        onChange={(e) => setGenre(e.target.value)}
-      />
-      <button type="submit">Add Poems </button>
-    </form>
+      <form onSubmit={handleSubmit}>
+        {error && <p className="error-message">{error}</p>} 
+
+      
+        <input
+          type="text"
+          placeholder="Poem Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Genre"
+          value={genre}
+          onChange={(e) => setGenre(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button type="submit" className="btn-add"> Add Poems </button>
+      </form>
     </div>
   );
 };
